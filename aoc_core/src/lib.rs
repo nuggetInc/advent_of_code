a48a16bc2a3d5672fb80beabffbbf94d1a4c69bc
NuggetInc <cdl.0404@gmail.com>
2023-12-09 09:50:40 +0100

    new way of getting the session cookie

--- aoc_core/src/lib.rs
+++ aoc_core/src/lib.rs
@@ -2,18 +2,19 @@ mod day;
 mod result;
 mod year;
 
-use std::{fs::File, io::Write};
+use std::{env, fs::File, io::Write};
 
 pub use day::Day;
 use scraper::{Html, Selector};
 pub use year::{Year, YearDay};
 
-
 pub fn download_problem(year: u32, day: YearDay) {
     let client = reqwest::blocking::Client::new();
 
+    let cookie = env::var("AOC_SESSION").expect("AOC_SESSION was not set");
+
     let url = format!("https://adventofcode.com/{}/day/{}", year, u32::from(day));
-    let response = client.get(&url).header("Cookie", COOKIE).send().unwrap();
+    let response = client.get(&url).header("Cookie", cookie).send().unwrap();
     let text = response.text().unwrap();
 
     let mut file = File::create(format!(
